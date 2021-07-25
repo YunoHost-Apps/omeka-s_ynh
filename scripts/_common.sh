b51fcecb554f29afe7d105fa7269ff387d26b4a0
ericgaspar <junk.eg@free.fr>
2021-07-25 13:44:34 +0200

    First commit

---
+++ scripts/_common.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+YNH_PHP_VERSION="7.3"
+
+extra_php_dependencies="php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml"
+
+# dependencies used by the app
+pkg_dependencies="imagemagick"
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
