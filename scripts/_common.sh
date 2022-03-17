800ca5f6c34af04007e8f028c388808ed12dc16f
ericgaspar <junk.eg@free.fr>
2022-03-17 16:27:07 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,10 @@
 
 YNH_PHP_VERSION="7.3"
 
-pkg_dependencies="imagemagick php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml"
+# Composer version
+YNH_COMPOSER_VERSION="2.1.3"
+
+pkg_dependencies="imagemagick php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml"
 
 #=================================================
 # PERSONAL HELPERS
