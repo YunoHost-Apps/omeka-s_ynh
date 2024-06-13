f058033ef8c80aeb6fae386a91a26e1d271d81ea
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-06-13 12:57:40 +0200

    fix

---
+++ conf/local.config.php
@@ -0,0 +1,47 @@
+<?php
+return [
+    'logger' => [
+        'log' => false,
+        'priority' => \Laminas\Log\Logger::NOTICE,
+    ],
+    'http_client' => [
+        'sslcapath' => null,
+        'sslcafile' => null,
+    ],
+    'cli' => [
+        'phpcli_path' => null,
+    ],
+    'thumbnails' => [
+        'types' => [
+            'large' => ['constraint' => 800],
+            'medium' => ['constraint' => 200],
+            'square' => ['constraint' => 200],
+        ],
+        'thumbnailer_options' => [
+            'imagemagick_dir' => null,
+        ],
+    ],
+    'translator' => [
+        'locale' => 'en_US',
+    ],
+    'service_manager' => [
+        'aliases' => [
+            'Omeka\File\Store' => 'Omeka\File\Store\Local',
+            'Omeka\File\Thumbnailer' => 'Omeka\File\Thumbnailer\ImageMagick',
+        ],
+    ],
+    'ldap' => [
+        'adapter_options' => [
+            'server1' => [
+                'host' => 'localhost',
+                'username' => 'ou=users,dc=yunohost,dc=org',
+                'password' => null,
+                'bindRequiresDn' => true,
+                'baseDn' => 'ou=users,dc=yunohost,dc=org',
+                'accountFilterFormat' => "(&(|(objectclass=posixAccount))(uid={{username}})(permission=cn=__APP__.main,ou=permission,dc=yunohost,dc=org))",
+                'accountCanonicalForm' => 4,
+                'accountDomainName' => 'example.com',
+            ],
+        ],
+    ],
+];
